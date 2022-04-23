8f37253362ff1bcb993619a8dc141fe1ddd4957e
caaatisgood <almigod0718@gmail.com>
2022-04-23 15:52:01 +0200

    re-init with nft school's e2e example

---
+++ packages/react-app/src/components/CodeMinter.jsx
@@ -0,0 +1,184 @@
+import React, { useState } from "react";
+import { NFTStorage } from 'nft.storage';
+import { Card, Input, Upload, Button } from "antd";
+import { LoadingOutlined, PlusOutlined } from '@ant-design/icons';
+import { useContractLoader } from "../hooks";
+import Account from "./Account";
+import { Transactor } from "../helpers";
+import { NFT_STORAGE_KEY, CODE_NFT_IMAGE_CID, DEFAULT_CONTRACT_NAME } from "../constants";
+
+async function mintNFT({ contract, ownerAddress, provider, gasPrice, setStatus, image, name, code }) {
+  const metadataURI = CODE_NFT_IMAGE_CID;
+  const client = new NFTStorage({ token: NFT_STORAGE_KEY });
+  setStatus("Uploading to nft.storage...");
+  const metadata = await client.store({
+    name,
+    image,
+    properties: {
+      code,
+    },
+  });
+  setStatus(`Upload complete! Minting token with metadata URI: ${metadata.url}`);
+  // scaffold-eth's Transactor helper gives us a nice UI popup when a transaction is sent
+  const transactor = Transactor(provider, gasPrice);
+  const tx = await transactor(contract.mintToken(ownerAddress, metadataURI));
+
+  setStatus("Blockchain transaction sent, waiting confirmation...");
+
+  // Wait for the transaction to be confirmed, then get the token ID out of the emitted Transfer event.
+  const receipt = await tx.wait();
+  let tokenId = null;
+  for (const event of receipt.events) {
+    if (event.event !== 'Transfer') {
+        continue
+    }
+    tokenId = event.args.tokenId.toString();
+    break;
+  }
+  setStatus(`Minted token #${tokenId}`);
+  return tokenId;
+}
+
+export default function Minter({
+  customContract,
+  account,
+  gasPrice,
+  signer,
+  provider,
+  name,
+  price,
+  blockExplorer,
+}) {
+  const contracts = useContractLoader(signer);
+  let contract;
+  if (!name) {
+    name = DEFAULT_CONTRACT_NAME;
+  }
+  if (!customContract) {
+    contract = contracts ? contracts[name] : "";
+  } else {
+    contract = customContract;
+  }
+
+  const address = contract ? contract.address : "";
+
+  const [file, setFile] = useState(null);
+  const [previewURL, setPreviewURL] = useState(null);
+  const [nftName, setName] = useState("");
+  const [code, setCode] = useState("");
+  const [minting, setMinting] = useState(false);
+  const [status, setStatus] = useState("");
+  const [tokenId, setTokenId] = useState(null);
+
+  const beforeUpload = (file, fileList) => {
+    console.log(file, fileList);
+    setFile(file);
+    setPreviewURL(URL.createObjectURL(file));
+    return false;
+  }
+
+  const uploadButton = (
+    <div>
+      <PlusOutlined />
+      <div style={{ marginTop: 8 }}>
+        Choose image
+      </div>
+    </div>
+  );
+
+  const uploadView = (
+    <div>
+      Drop an image file or click below to select.
+      <Upload
+        name="avatar"
+        accept=".jpeg,.jpg,.png,.gif"
+        listType="picture-card"
+        className="avatar-uploader"
+        showUploadList={false}
+        action="https://www.mocky.io/v2/5cc8019d300000980a055e76"
+        beforeUpload={beforeUpload}
+      >
+        {uploadButton}
+      </Upload>
+    </div>
+  );
+
+  const preview = previewURL ? <img src={previewURL} style={{maxWidth: "800px"}}/> : <div/>
+
+  const nameField = (
+    <Input placeholder="Enter a name for your code" onChange={e => {
+      setName(e.target.value);
+    }}/>
+  );
+
+  const codeField = (
+    <Input.TextArea placeholder="Enter your code here" onChange={e => {
+      setCode(e.target.value);
+    }}/>
+  );
+
+  const mintEnabled = file != null && !!nftName;
+
+  const startMinting = () => {
+    console.log(`minting nft with name ${nftName}`);
+    setMinting(true);
+    signer.getAddress().then(ownerAddress => {
+      mintNFT({ 
+        contract, 
+        provider, 
+        ownerAddress, 
+        gasPrice, 
+        setStatus,
+        name,
+        image: file,
+        code,
+      }).then(newTokenId => {
+        setMinting(false);
+        console.log('minting complete');
+        setTokenId(newTokenId);
+      })
+    });
+  }
+  
+  const mintButton = (
+    <Button type="primary" disabled={!mintEnabled} onClick={startMinting}>
+      {minting ? <LoadingOutlined/> : "Mint!"}
+    </Button>
+  )
+  
+  const minterForm = (
+    <div style={{ margin: "auto", width: "70vw" }}>
+      <Card
+        title={
+          <div>
+            {name}
+            <div style={{ float: "right" }}>
+              <Account
+                address={address}
+                localProvider={provider}
+                injectedProvider={provider}
+                mainnetProvider={provider}
+                price={price}
+                blockExplorer={blockExplorer}
+              />
+              {account}
+            </div>
+          </div>
+        }
+        size="large"
+        style={{ marginTop: 25, width: "100%" }}
+        loading={false}
+      >
+        { file == null && uploadView }
+        {preview}
+        {nameField}
+        {codeField}
+        {mintButton}
+        {status}
+      </Card>
+    </div>
+  );
+
+
+  return minterForm;
+}
--- packages/react-app/src/components/index.js
+++ packages/react-app/src/components/index.js
@@ -1,55 +1,20 @@
-import styled from "styled-components";
-
-export const Body = styled.div`
-  align-items: center;
-  color: white;
-  display: flex;
-  flex-direction: column;
-  font-size: calc(10px + 2vmin);
-  justify-content: center;
-  margin-top: 40px;
-`;
-
-export const Button = styled.button`
-  background-color: white;
-  border: none;
-  border-radius: 8px;
-  color: #282c34;
-  cursor: pointer;
-  font-size: 16px;
-  margin: 0px 20px;
-  padding: 12px 24px;
-  text-align: center;
-  text-decoration: none;
-`;
-
-export const Container = styled.div`
-  background-color: #282c34;
-  display: flex;
-  flex-direction: column;
-  height: calc(100vh);
-`;
-
-export const Header = styled.header`
-  align-items: center;
-  background-color: #282c34;
-  color: white;
-  display: flex;
-  flex-direction: row;
-  justify-content: flex-end;
-  min-height: 70px;
-`;
-
-export const Image = styled.img`
-  height: 40vmin;
-  margin-bottom: 16px;
-  pointer-events: none;
-`;
-
-export const Link = styled.a.attrs({
-  target: "_blank",
-  rel: "noopener noreferrer",
-})`
-  color: #61dafb;
-  margin-top: 8px;
-`;
+export { default as Account } from "./Account";
+export { default as Address } from "./Address";
+export { default as AddressInput } from "./AddressInput";
+export { default as Balance } from "./Balance";
+export { default as Blockie } from "./Blockie";
+export { default as BytesStringInput } from "./BytesStringInput";
+export { default as Contract } from "./Contract";
+export { default as EtherInput } from "./EtherInput";
+export { default as Faucet } from "./Faucet";
+export { default as GasGauge } from "./GasGauge";
+export { default as Header } from "./Header";
+export { default as CodeMinter } from "./CodeMinter";
+export { default as NFTViewer } from "./NFTViewer";
+export { default as Provider } from "./Provider";
+export { default as Ramp } from "./Ramp";
+export { default as Swap } from "./Swap";
+export { default as ThemeSwitch } from "./ThemeSwitch";
+export { default as Timeline } from "./Timeline";
+export { default as TokenBalance } from "./TokenBalance";
+export { default as Wallet } from "./Wallet";
